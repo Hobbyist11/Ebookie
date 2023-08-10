375e8196477dc07d526bedefbae7dc2a41151f1f
A <bitranjit123@gmail.com>
2023-08-11 06:03:01 +0800

    Added UI

--- main.go
+++ main.go
@@ -1,36 +1,115 @@
+// Initial UI for tabs
 package main
 
 import (
-    "fmt"
-    "os"
-    "path/filepath"
+	"fmt"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+type model struct {
+	Tabs       []string
+	TabContent []string
+	activeTab  int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "right", "l", "n", "tab":
+			m.activeTab = min(m.activeTab+1, len(m.Tabs)-1)
+			return m, nil
+		case "left", "h", "p", "shift+tab":
+			m.activeTab = max(m.activeTab-1, 0)
+			return m, nil
+		}
+	}
+
+	return m, nil
+}
+
+func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
+	border := lipgloss.RoundedBorder()
+	border.BottomLeft = left
+	border.Bottom = middle
+	border.BottomRight = right
+	return border
+}
+
+var (
+	inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
+	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
+	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
+	activeTabStyle    = inactiveTabStyle.Copy().Border(activeTabBorder, true)
+	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Center).Border(lipgloss.NormalBorder()).UnsetBorderTop()
+)
+
+func (m model) View() string {
+	doc := strings.Builder{}
+
+	var renderedTabs []string
+
+	for i, t := range m.Tabs {
+		var style lipgloss.Style
+		isFirst, isLast, isActive := i == 0, i == len(m.Tabs)-1, i == m.activeTab
+		if isActive {
+			style = activeTabStyle.Copy()
+		} else {
+			style = inactiveTabStyle.Copy()
+		}
+		border, _, _, _, _ := style.GetBorder()
+		if isFirst && isActive {
+			border.BottomLeft = "│"
+		} else if isFirst && !isActive {
+			border.BottomLeft = "├"
+		} else if isLast && isActive {
+			border.BottomRight = "│"
+		} else if isLast && !isActive {
+			border.BottomRight = "┤"
+		}
+		style = style.Border(border)
+		renderedTabs = append(renderedTabs, style.Render(t))
+	}
+
+	row := lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...)
+	doc.WriteString(row)
+	doc.WriteString("\n")
+	doc.WriteString(windowStyle.Width((lipgloss.Width(row) - windowStyle.GetHorizontalFrameSize())).Render(m.TabContent[m.activeTab]))
+	return docStyle.Render(doc.String())
+}
+
 func main() {
-    // Get the current directory.
-    dir, err := os.Getwd()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	tabs := []string{"UnRead", "Read"}
+	tabContent := []string{"UnRead books go here..", "Read books go here"}
+	m := model{Tabs: tabs, TabContent: tabContent}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
 
-    // Create a channel to store the .epub and .pdf files.
-    epubsAndPdfs := make(chan string)
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
-    // Go through all the files in the current directory.
-    for _, file := range filepath.Glob("*") {
-        // Check if the file is an .epub or .pdf file.
-        if filepath.Ext(file) == ".epub" || filepath.Ext(file) == ".pdf" {
-            // Send the file to the channel.
-            epubsAndPdfs <- file
-        }
-    }
-
-    // Close the channel.
-    close(epubsAndPdfs)
-
-    // Iterate over the channel and print the .epub and .pdf files.
-    for file := range epubsAndPdfs {
-        fmt.Println(file)
-    }
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
