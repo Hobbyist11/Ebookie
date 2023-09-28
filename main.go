9b547d5269dd248dad25ddc65eeb417b616b4907
A <bitranjit123@gmail.com>
2023-09-28 21:00:41 +0800

    Boiler code

--- main.go
+++ main.go
@@ -1,35 +1,37 @@
-// import
-import(
-  "fmt"
-"os")
-// We need to scan the FS
+package main
 
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+)
 
+func main() {
+    // Get the current directory.
+    dir, err := os.Getwd()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
 
+    // Create a channel to store the .epub and .pdf files.
+    epubsAndPdfs := make(chan string)
 
+    // Go through all the files in the current directory.
+    for _, file := range filepath.Glob("*") {
+        // Check if the file is an .epub or .pdf file.
+        if filepath.Ext(file) == ".epub" || filepath.Ext(file) == ".pdf" {
+            // Send the file to the channel.
+            epubsAndPdfs <- file
+        }
+    }
 
+    // Close the channel.
+    close(epubsAndPdfs)
 
-//Find epubs and PDFS
+    // Iterate over the channel and print the .epub and .pdf files.
+    for file := range epubsAndPdfs {
+        fmt.Println(file)
+    }
+}
 
-
-
-// List epubs and PDFS
-
-
-
-
-
-// Group them by tags
-
-
-
-
-//Add tag function
-
-
-//Edit tag function
-
-
-
-
-// Sort by tags;
